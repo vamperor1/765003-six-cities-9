6e486744da2acd55e19b649d94ee61c8086910c4
vamperor1 <vamperor@mail.ru>
2022-02-18 14:29:35 +0300

    2. Добавит React-компоненты

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,8 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
 
+const Setting = {
+  PLACE_CARDS_COUNT: 5,
+};
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App placeCardsCount = {Setting.PLACE_CARDS_COUNT}/>
   </React.StrictMode>,
   document.getElementById('root'));
+
