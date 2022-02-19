e2a16e4758a1693163e203273dd1327b8e2049ba
vamperor1 <vamperor@mail.ru>
2022-02-19 22:47:25 +0300

    Исправит закрывающий тег input

--- project/src/components/property-reviews-form/property-reviews-form.tsx
+++ project/src/components/property-reviews-form/property-reviews-form.tsx
@@ -10,28 +10,28 @@ function PropertyReviewsForm(): JSX.Element {
           </svg>
         </label>
 
-        <input className="form__rating-input visually-hidden" name="rating" value="4" id="4-stars" type="radio"/>
+        <input className="form__rating-input visually-hidden" name="rating" value="4" id="4-stars" type="radio" />
         <label htmlFor="4-stars" className="reviews__rating-label form__rating-label" title="good">
           <svg className="form__star-image" width="37" height="33">
             <use xlinkHref="#icon-star"></use>
           </svg>
         </label>
 
-        <input className="form__rating-input visually-hidden" name="rating" value="3" id="3-stars" type="radio"/>
+        <input className="form__rating-input visually-hidden" name="rating" value="3" id="3-stars" type="radio" />
         <label htmlFor="3-stars" className="reviews__rating-label form__rating-label" title="not bad">
           <svg className="form__star-image" width="37" height="33">
             <use xlinkHref="#icon-star"></use>
           </svg>
         </label>
 
-        <input className="form__rating-input visually-hidden" name="rating" value="2" id="2-stars" type="radio"/>
+        <input className="form__rating-input visually-hidden" name="rating" value="2" id="2-stars" type="radio" />
         <label htmlFor="2-stars" className="reviews__rating-label form__rating-label" title="badly">
           <svg className="form__star-image" width="37" height="33">
             <use xlinkHref="#icon-star"></use>
           </svg>
         </label>
 
-        <input className="form__rating-input visually-hidden" name="rating" value="1" id="1-star" type="radio"/>
+        <input className="form__rating-input visually-hidden" name="rating" value="1" id="1-star" type="radio" />
         <label htmlFor="1-star" className="reviews__rating-label form__rating-label" title="terribly">
           <svg className="form__star-image" width="37" height="33">
             <use xlinkHref="#icon-star"></use>
