88440d35e547ca9eeb6398427e5532e4abe789d5
vamperor1 <vamperor@mail.ru>
2022-07-30 23:23:38 +0300

    Задание 8. Финальные правки

--- project/src/components/main-screen/main-screen.tsx
+++ project/src/components/main-screen/main-screen.tsx
@@ -7,25 +7,25 @@ import {getSortedOffers} from '../../store/offers-data/selectors';
 
 function MainScreen(): JSX.Element {
   const sortedOffers = useAppSelector(getSortedOffers);
-  const isEmpty = sortedOffers.length > 0;
+  const isEmpty = sortedOffers.length === 0;
 
   return (
     <div className="page page--gray page--main">
       <Header />
-      <main className={isEmpty ? 'page__main page__main--index'
-        : 'page__main page__main--index page__main--index-empty'}
+      <main className={isEmpty ? 'page__main page__main--index page__main--index-empty'
+        : 'page__main page__main--index'}
       >
         <h1 className="visually-hidden">Cities</h1>
         <CitiesTabs />
         <div className="cities">
           <div className={isEmpty
-            ? 'cities__places-container container'
-            : 'cities__places-container cities__places-container--empty container'}
+            ? 'cities__places-container cities__places-container--empty container'
+            : 'cities__places-container container'}
           >
             {
               isEmpty
-                ? <CitiesPlaces offers={sortedOffers}/>
-                : <MainScreenEmpty />
+                ? <MainScreenEmpty />
+                : <CitiesPlaces offers={sortedOffers}/>
             }
           </div>
         </div>
